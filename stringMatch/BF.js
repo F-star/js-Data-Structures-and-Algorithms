9a9d43c0f241df82294a857392f01a1fe02ae4a5
fstar <huanghao>
2019-02-23 13:55:39 +0800

    BF字符串匹配算法

---
+++ stringMatch/BF.js
@@ -0,0 +1,31 @@
+
+
+
+/**
+ * BF算法
+ * 在字符串 s 中找 pattern。
+ * 
+ * @param {string} s 主串
+ * @param {string} pattern 模式串
+ */
+export const BF = (s, pattern) => {
+
+    const m = pattern.length,
+        n = s.length;
+    if (n < m) return -1;
+    // 。。。
+    for (let i = 0, len = n - m + 1; i < len; i++) {
+        
+   
+        for (let j = 0; j < m; j++) {
+            if (pattern[j] !== s[i + j]) {
+                break;   // 发现不匹配，结束当前循环
+            } 
+            // 匹配到模式串的最后一个字符，说明字符串匹配成功。
+            else if (j == m - 1) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
---
+++ stringMatch/test.js
@@ -0,0 +1,9 @@
+import { BF } from "./BF.js";
+
+
+console.log('-------------- BF 算法测试');
+const a = 'ldkojkladk;djoald0faa00faa'
+const b = 'faa';
+console.log('s:      ', a)
+console.log('pattern:', b);
+console.log(BF(a, b));
